68f336501dd7e1a3993556dcc624b2e06d21a49d
Nastasya.Churashova <gregarious@list.ru>
2021-02-17 12:17:39 +0200

    git branch -M main

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <link rel="stylesheet" href="style.css">
 
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
 <?php $entries = [
     ['', 'x', ''],
     ['x', '', ''],
@@ -7,13 +13,13 @@
 ];
  ?>
 
-<div class="container">
+<table>
 <?php for ($row = 0; $row < 3; $row++): ?>
       <?php for($col = 0; $col < 3; $col++): ?>
-    <a href="#"><?=$row . "/" . $col; ?></a>
+    <a href="#"><?=$entries[$row][$col]; ?></a>
     <?php endfor; ?>
 <?php endfor; ?>
-</div>
+      </table>
 
 <pre><?=print_r($entries, true)
 ?></pre>
