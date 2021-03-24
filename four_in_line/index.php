6326eabed8616d8ebc4114ebf01c4ea07cb7d25d
Nastasya.Churashova <gregarious@list.ru>
2021-03-24 10:46:35 +0200

    long version with win combinations

--- four_in_line/index.php
+++ four_in_line/index.php
@@ -27,35 +27,119 @@ if (array_key_exists('r', $_REQUEST) && array_key_exists('c', $_REQUEST)) {
     $c = $_REQUEST['c'];
     echo "<h3>r=" . $r . "; c= " . $c . "</h3>";
       
-        if (
-            !array_key_exists($r, $table) ||
-            !array_key_exists($c, $table[$r]) ||
-            $table[$r][$c] === ''
-        ) {
-            $entries['count'] = array_key_exists('count', $entries) ? $entries['count'] + 1 : 1;
-            $table[$r][$c] = $entries['count'] % 2 === 0 ? 'o' : "x";
+    if (
+        !array_key_exists($r, $table) ||
+        !array_key_exists($c, $table[$r]) ||
+        $table[$r][$c] === ''
+    ) {
+        $entries['count'] = array_key_exists('count', $entries) ? $entries['count'] + 1 : 1;
+        
+        if (@$table[$r+1][$c] !='' || $r==9){
+            $value = $entries['count'] % 2 === 0 ? 'o' : "x";
+            $table[$r][$c] = $value;
             saveEntries($entries);
+        
+            if (@$table[$r][$c+1]==$value && 
+                @$table[$r][$c+2] ==$value && 
+                @$table[$r][$c+3] ==$value){
+                    echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r][$c-1]==$value && 
+                @$table[$r][$c-2] ==$value && 
+                @$table[$r][$c-3] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r][$c-1]==$value && 
+                @$table[$r][$c+1] ==$value && 
+                @$table[$r][$c+2] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r][$c-2] ==$value &&
+                @$table[$r][$c-1]==$value  &&
+                @$table[$r][$c+1] ==$value ){
+                echo  "The winner is" . PHP_EOL . $value;
+            }//new 'if' for vertical
+            if (@$table[$r+1][$c] ==$value &&
+                @$table[$r+2][$c]==$value  &&
+                @$table[$r+3][$c] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r-1][$c] ==$value &&
+                @$table[$r-2][$c]==$value  &&
+                @$table[$r-3][$c] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r-1][$c] ==$value &&
+                @$table[$r+1][$c]==$value  &&
+                @$table[$r+2][$c] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r-2][$c] ==$value &&
+                @$table[$r-1][$c]==$value  &&
+                @$table[$r+1][$c] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            //new 'īf' for diagonal
+            if (@$table[$r+1][$c+1] ==$value &&
+                @$table[$r+2][$c+2]==$value  &&
+                @$table[$r+3][$c+3] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r-1][$c-1] ==$value &&
+                @$table[$r+1][$c+1]==$value  &&
+                @$table[$r+2][$c+2] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r-1][$c-1] ==$value &&
+                @$table[$r-2][$c-2]==$value  &&
+                @$table[$r-3][$c-3] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r-2][$c-2] ==$value &&
+                @$table[$r-1][$c-1]==$value  &&
+                @$table[$r+1][$c+1] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
+            if (@$table[$r+1][$c-1] ==$value &&
+                @$table[$r+2][$c-2]==$value  &&
+                @$table[$r+3][$c-3] ==$value){
+                echo  "The winner is" . PHP_EOL . $value;
+            }
         }
     }
+}
 
+/* attempt to optimize code
 
-$r = $_REQUEST['r'];
-$c = $_REQUEST['c'];
+        for ($c=0; $c<=9; $c++){
+            for ($r=9; $r>0; $r--){
+                $win_combinations = [
+                    [$r, $c, $r-1, $c, $r-2, $c, $r-3, $c],
+                    [$r, $c, $r, $c+1, $r, $c+2, $r, $c+3]
+                ];
+                for ($i = 0; $i < count($win_combinations); $i++) {
+                    $r1 = $win_combinations[$i][0];
+                    $c1 = $win_combinations[$i][1];
 
-if (array_key_exists($r, $table) && array_key_exists($c, $table[$r]) && array_key_exists('count', $entries) && $table[$r][$c] = 'o' || "x" && $r=9) {
-    $table[$r][$c] ='';
-  }
+                    if (getEntry($table, $r1, $c1) == $value
+                        ){
+                            echo  "The winner is" . PHP_EOL . $value;
+                        }
+                    }
+            }
+        } 
+      
+    }
+}
+*/
 ?>
-
-
-<link rel="stylesheet" href="style.css">
 
 <div class="container">
     <?php
     for ($r = 0; $r <= 9; $r++) {
         for ($c = 0; $c <= 9; $c++) {
             $value = (array_key_exists($r, $table) &&
-                array_key_exists($c, $table[$r])) ? $table[$r][$c] : '';
+                     array_key_exists($c, $table[$r])) ? $table[$r][$c] : '';
             echo "<a href='?r=$r&c=$c'>$value</a>";
         }
     }
